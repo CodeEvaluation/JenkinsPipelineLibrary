dcadca6e88fbd5ce2a2d0f85ab99e873f335adab
romo <robmoore121@gmail.com>
2020-12-31 14:51:07 +0800

    Rename parameter and field to match type.

--- src/stage/analysis/AnalysisStage.groovy
+++ src/stage/analysis/AnalysisStage.groovy
@@ -4,19 +4,19 @@ import pipeline.Jenkins
 
 class AnalysisStage {
     private final Jenkins jenkins
-    private final JavaFileFinder finder
+    private final JavaFileFinder javaFileFinder
     private final Analyser analyser
 
-    AnalysisStage(Jenkins jenkins, JavaFileFinder javaClassFinder, Analyser analyser) {
+    AnalysisStage(Jenkins jenkins, JavaFileFinder javaFileFinder, Analyser analyser) {
         this.jenkins = jenkins
-        this.finder = javaClassFinder
+        this.javaFileFinder = javaFileFinder
         this.analyser = analyser
     }
 
     void run(String codeDirectoryAbsolutePath) {
         jenkins.stage("Analysis") {
             jenkins.println("codeDirectoryAbsolutePath: ${codeDirectoryAbsolutePath}")
-            List<String> sourceClasses = finder.findSourceFilePaths(codeDirectoryAbsolutePath)
+            List<String> sourceClasses = javaFileFinder.findSourceFilePaths(codeDirectoryAbsolutePath)
             jenkins.println("sourceClasses: ${sourceClasses.toString()}")
             jenkins.dir(codeDirectoryAbsolutePath) {
                 createReport(sourceClasses)
